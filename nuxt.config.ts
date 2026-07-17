8130153a7a7619a3a3fb8bbb08ebc08a545c8168
Valery Atamanov <i@freshnet.ru>
2026-07-17 20:19:03 +0300

    config and packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+
+  css: [
+    "~/assets/css/resets.css",
+    "~/assets/css/font.css",
+    "~/assets/css/vars.css",
+    "~/assets/css/main.css",
+  ],
+
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/image"],
+});
