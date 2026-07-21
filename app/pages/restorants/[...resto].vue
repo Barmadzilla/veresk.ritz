482cde604d0b0e88e6cfdd972d060e1338c0a29e
Valery Atamanov <i@freshnet.ru>
2026-07-22 01:37:20 +0300

    Finishing restorants pages

--- app/pages/restorants/[...resto].vue
+++ app/pages/restorants/[...resto].vue
@@ -1,12 +1,61 @@
 <template>
   <div>
-    <BreadCrumbs current="Title" />
-    <TitleMain title="restoran" />
+    <BreadCrumbs :current="data.title" />
+    <TitleMain :title="data.title" />
+    <ContentParams>Ежедневно {{ workingHours }}</ContentParams>
+    <ButtonGroup>
+      <ButtonIconNude icon="banquet" label="Меню" to="/" />
+      <ButtonSolid label="Забронировать Стол" to="/" />
+    </ButtonGroup>
+    <ContentTextBlock :data="list.description" />
+    <ButtonGroup>
+      <ButtonRoomMedia type="virtual-tour" />
+      <ButtonRoomMedia type="video" />
+      <ButtonRoomMedia type="gallery" />
+    </ButtonGroup>
+    <component
+      v-for="(item, index) in data.content"
+      :key="index"
+      :is="
+        item.type == 'textAndPic'
+          ? TextAndPic
+          : item.type == 'textBlock'
+            ? TextBlock
+            : item.type == 'slideShow'
+              ? SlideShow
+              : null
+      "
+      v-bind="
+        item.type == 'slideShow'
+          ? { type: 'image', cards: 1, data: item.slideShow }
+          : { data: item, reverse: index % 2 == 0 }
+      "
+    />
+    <More />
   </div>
 </template>
 
 <script setup>
 const route = useRoute();
+
+const TextAndPic = resolveComponent("ContentTextAndPic");
+const TextBlock = resolveComponent("ContentTextBlock");
+const SlideShow = resolveComponent("SliderContainer");
+
+const listData = restorantsData();
+const list = listData.value.find((item) => item.slug == route.params.resto[0]);
+const workingHours = fromTo(list.working.from, list.working.to);
+
+const content = {
+  veresk: vereskData(),
+  primavera: primaveraData(),
+};
+
+const data = ref(content[route.params.resto[0]]);
+
+const poster = setPoster();
+poster.value = "/images/posters/restorants.jpg";
+
 definePageMeta({
   parent: [{ title: "Рестораны", slug: "/restorants" }],
 });
--- app/pages/restorants/index.vue
+++ app/pages/restorants/index.vue
@@ -16,9 +16,11 @@
 const title = "Рестораны спа-курорта вереск";
 const text =
   "Наш ресторан «Вереск» в каждой детали вдохновлен чистотой и естественностью природы. Мы постарались создать уютное пространство наедине с природой: панорамный вид на сосновый лес и берег озера, оборудованные террасы и спуски к воде, много дерева в интерьерах и комфорт в каждой детали.";
+const poster = setPoster();
+poster.value = "/images/posters/restorants.jpg";
+
 definePageMeta({
   title: "Рестораны",
-  poster: "/images/posters/restorants.jpg",
 });
 const listData = restorantsData();
 </script>
